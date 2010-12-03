040477c7a386f503f5fcfe6fe2f3b3eaf232de6c
cuneytcalishkan <cuneytcalishkan@a7b2f418-de0d-a752-9495-567de09dda26>
2010-12-03 12:56:04 +0000

    FISH Client

---
+++ FISHClient/src/client/FileDownloader.java
@@ -0,0 +1,124 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import util.Helper;
+
+/**
+ * Downloads the specified file from the given server on the given port and saves
+ * the downloaded file to the specified save path. If the save path is the same place
+ * where the given client shares files, the server is called for an update of the
+ * shared files.
+ * @author CUNEYT
+ */
+public class FileDownloader implements Runnable {
+
+    private String fileName;
+    private String server;
+    private int port;
+    private Socket socket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private String savePath;
+    private FileOutputStream osw;
+    private InputStream isr;
+    private Client c;
+
+    /**
+     * Downloads the specified file from the given server on the given port and saves
+     * the downloaded file to the specified save path. If the save path is the same place
+     * where the given client shares files, the server is called for an update of the
+     * shared files.
+     * @param file the file name to download
+     * @param server the server IP address
+     * @param p the server's port number
+     * @param savePath the path where to save the downloaded file
+     * @param client the <code>Client</code> that downloads the file
+     */
+    public FileDownloader(String file, String server, int p, String savePath, Client client) {
+        this.fileName = file;
+        this.server = server;
+        this.port = p;
+        this.savePath = savePath;
+        this.c = client;
+    }
+
+    public void run() {
+        try {
+            socket = new Socket(server, port);
+            reader = Helper.getBufferedReader(socket);
+            writer = Helper.getPrintWriter(socket);
+            writer.println(fileName);
+            String found = reader.readLine();
+            if (found.equals(Helper.NOT_FOUND)) {
+                System.out.println("\n" + fileName + " is not shared any more.");
+                closeConnection();
+                return;
+            }
+            long length = Long.parseLong(reader.readLine());
+            fileName = savePath + File.separator + fileName;
+            osw = new FileOutputStream(fileName);
+            isr = socket.getInputStream();
+            System.out.println("\nDownloading file to " + fileName);
+
+            int bufSize = 1024;
+            long remaining = length;
+            if (remaining < bufSize) {
+                bufSize = (int) remaining;
+            }
+            int result;
+            byte[] buf = new byte[bufSize];
+            while ((result = isr.read(buf)) != -1) {
+                osw.write(buf);
+                remaining -= result;
+                if (remaining < bufSize && remaining > 0) {
+                    bufSize = (int) remaining;
+                }
+                buf = new byte[bufSize];
+            }
+            System.out.println("\nFinished downloading " + fileName);
+            if (savePath.equals(c.getBasePath())) {
+                c.updateSharedFiles();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(FileDownloader.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            closeConnection();
+        }
+    }
+
+    /**
+     * Closes the necessary input and output stream readers and writers related to the connection or file.
+     */
+    private void closeConnection() {
+        try {
+            if (reader != null) {
+                reader.close();
+            }
+            if (writer != null) {
+                writer.flush();
+                writer.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+            if (osw != null) {
+                osw.flush();
+                osw.close();
+            }
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+}
---
+++ FISHClient/src/client/FileServer.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import server.FileUploader;
+
+/**
+ *
+ * @author CUNEYT
+ */
+public class FileServer extends Thread {
+
+    private int port;
+    private String basePath;
+    private Executor executor;
+    private boolean done = false;
+
+    /**
+     * Listens for the connections coming to the specified port number and creates
+     * a new <code>FileUploader</code> which is executed in a cached thread pool.
+     * @param basePath the base path to the shared files
+     * @param p the port number on which to listen
+     */
+    public FileServer(String basePath, int p) {
+        this.basePath = basePath;
+        this.port = p;
+        this.executor = Executors.newCachedThreadPool();
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket server = new ServerSocket(port);
+            while (!done) {
+                Socket s = server.accept();
+                FileUploader uploader = new FileUploader(s, basePath);
+                executor.execute(uploader);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
